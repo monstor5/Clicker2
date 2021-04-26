12a555ad2eaa05b602deb902d088c5786a33dcde
Arsenii324 <59286449+Arsenii324@users.noreply.github.com>
2021-04-26 08:24:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/clicker/GameActivity.kt
@@ -0,0 +1,24 @@
+package com.example.clicker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class GameActivity : AppCompatActivity() {
+
+
+    var coutmoney = 0
+    var money = 100
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            findViewById<TextView>(R.id.countTextView).text = coutmoney.toString()
+            coutmoney++
+        }
+    }
+}
